c1229aad72648c70c7cdf7afc6226639e4c2f150
mehrdad <mehrdadosoulisaran@gmail.com>
2024-01-14 11:17:37 +0330

    add folder structure and add nessecory file css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import './App.css'
+import MainPage from './pages/mainPage/mainPage'
 
 function App() {
-
   return (
     <>
-    <h1 className="text-3xl text-red-200 font-bold underline">
-      Hello world!
-    </h1>
+      {
+        
+      }      
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import './styles/reset.css';
+import './styles/fonts.css';
+import './styles/variables.css';
+import './styles/defaults.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>,
 )
