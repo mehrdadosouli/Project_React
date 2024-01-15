647825a065a19d9641bc7e2d2df0fa588818ceae
mehrdad <mehrdadosoulisaran@gmail.com>
2024-01-15 16:52:16 +0330

    set search and set usenavigate for search in landing

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Typewriter from 'typewriter-effect';
+import { IoSearchOutline } from "react-icons/io5";
+import { useNavigate } from 'react-router-dom';
 
 export default function Landing() {
-    
-
+    const [search,setSearch]=useState('')
+    const navigate=useNavigate()
+    const searchHandler=(e)=>{
+        navigate(`/search/${search}`)
+    }
 
   return (
     <>
-        <div className='sticky bg-bg_landing w-full h-[18rem] sm:h-[52.5rem] md:h-[61.9rem] bg-contain bg-top bg-no-repeat sm:bg-cover sm:bg-center bg-fixed'>
+        <div className='sticky bg-bg_landing w-full h-[15rem] sm:h-[52.5rem] md:h-[61.9rem] bg-contain bg-top bg-no-repeat sm:bg-cover sm:bg-center bg-fixed'>
             <h1 className="landing__title absolute lg:top-[60%] lg:left-[30%] top-[60%] left-[20%] text-white md:text-6xl sm:text-4xl text-2xl">
             <Typewriter
                 onInit={(typeWriter) => {
@@ -25,6 +30,11 @@ export default function Landing() {
                 }}
             />
             </h1>
+   
+                <div className='relative sm:top-[50%] top-[30%] w-[50%] h-[3rem] mx-auto'>
+                    <input className='absolute left-0 right-0 top-0 bottom-0 p-5 rounded-lg' type="text" value={search} onChange={(e)=>setSearch(e.target.value)} />
+                    <span className='absolute left-[2%] bottom-0 top-[20%] text-green-400'><IoSearchOutline className='hover:cursor-pointer' onClick={searchHandler}/></span>
+            </div>
         </div>
         
     </>
